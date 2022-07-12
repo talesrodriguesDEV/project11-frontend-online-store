ad3935c6f2cf9553ad4d2b1a6df939b432660dbc
Tales Rodrigues <tales00001@gmail.com>
2022-07-12 16:42:48 -0300

    Updates

---
+++ precache-manifest.d20b4b7fd77c2c5c13881d72c4ef6030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8acd321f5bcf47f83abba2db844f2f18",
+    "url": "/project11-frontend-online-store/index.html"
+  },
+  {
+    "revision": "05513facac1a3ec37e39",
+    "url": "/project11-frontend-online-store/static/css/main.0e54bf57.chunk.css"
+  },
+  {
+    "revision": "4de4be4878a27c42a159",
+    "url": "/project11-frontend-online-store/static/js/2.4aeaaec7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project11-frontend-online-store/static/js/2.4aeaaec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05513facac1a3ec37e39",
+    "url": "/project11-frontend-online-store/static/js/main.71537636.chunk.js"
+  },
+  {
+    "revision": "bb455ebdef687dbd5d4f",
+    "url": "/project11-frontend-online-store/static/js/runtime-main.eaafb61e.js"
+  }
+]);
